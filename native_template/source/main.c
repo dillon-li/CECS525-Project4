beef272edb6175d7363861bec539f9831abd8344
Davis <davisdavis27@gmail.com>
2017-11-27 13:23:28 -0500

    Add files via upload

---
+++ native_template/source/main.c
@@ -0,0 +1,546 @@
+// main.c - main for the CECS 525 Raspberry PI kernel.img
+// by Eugene Rockey Copyright 2013 All Rights Reserved
+// debug everything that needs debugging
+// Add, remove, modify, preserve in order to fulfill project 4 requirements.
+
+#include <stdint.h>
+#include <stddef.h>
+#include "uart.h"
+#include "mmio.h"
+#include "bcm2835.h"
+//#include "math.h"
+
+#define SECS 0x00
+#define MINS 0x01
+#define HRS	 0x02
+#define DOM	 0x04
+#define MONTH 0x05
+#define YEAR 0x06
+#define ASECS 0x07
+#define TEMP 0x11
+#define CR 0x0D
+
+
+const char MS1[] = "\r\n\nCECS-525 RPI Berry OS";
+const char MS2[] = "\r\nby Eugene Rockey Copyright 2013 All Rights Reserved";
+const char MS3[] = "\r\nReady: ";
+const char MS4[] = "\r\nInvalid Command Try Again...";
+
+//PWM Data for Alarm Tone
+uint32_t N[200] = {0,1,2,3,4,5,6,7,8,9,10,11,12,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,
+				36,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,60,61,62,63,64,65,66,67,68,69,
+				70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,84,85,86,87,88,89,90,91,92,93,94,95,96,95,94,93,92,91,90,
+				89,88,87,86,85,84,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,60,59,58,57,
+				56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,36,35,34,33,32,31,30,29,28,27,26,25,24,23,
+				   22,21,20,19,18,17,16,15,14,13,12,12,11,10,9,8,7,6,5,4,3,2,1};
+char* buffer[1];
+char* ISCN[1];
+char alarm[1];
+uint8_t ones;
+uint8_t tens;
+char* tbuf;
+char* rbuf;
+char* cbuf = '\0';
+void kernel_main();             //prototypes
+void enable_arm_irq();
+void disable_arm_irq();
+void enable_arm_fiq();
+void disable_arm_fiq();
+char read_char_buffer();
+void reboot();
+void enable_irq_57();
+void disable_irq_57();
+void enable_irq_52();
+void enable_1hz();
+void falling_edge(uint8_t);
+extern int invar;               //assembly variables
+extern int outvar;
+void displaycommandline();
+
+
+
+//Pointers to some of the BCM2835 peripheral register bases
+volatile uint32_t* bcm2835_gpio = (uint32_t*)BCM2835_GPIO_BASE;
+volatile uint32_t* bcm2835_clk = (uint32_t*)BCM2835_CLOCK_BASE;
+volatile uint32_t* bcm2835_pads = (uint32_t*)BCM2835_GPIO_PADS;//for later updates to program
+volatile uint32_t* bcm2835_spi0 = (uint32_t*)BCM2835_SPI0_BASE;
+volatile uint32_t* bcm2835_bsc0 = (uint32_t*)BCM2835_BSC0_BASE;//for later updates to program
+volatile uint32_t* bcm2835_bsc1 = (uint32_t*)BCM2835_BSC1_BASE;
+volatile uint32_t* bcm2835_st = (uint32_t*)BCM2835_ST_BASE;
+
+
+void falling_edge(uint8_t pin)
+{
+    volatile uint32_t* paddr = bcm2835_gpio + BCM2835_GPFEN0/4 + pin/32;
+    uint8_t shift = pin % 32;
+    uint32_t value = 1 << shift;
+    bcm2835_peri_set_bits(paddr, value, value);
+}
+
+void enable_irq_57(void)
+{
+	mmio_write(0x2000B214, 0x02000000);
+}
+
+void disable_irq_57(void)
+{
+	mmio_write(0x2000B220, 0x02000000);
+}
+
+void enable_irq_52(void)
+{
+	mmio_write(0x2000B214,0x00020000);
+	bcm2835_gpio_fsel(23,BCM2835_GPIO_FSEL_INPT);
+	bcm2835_gpio_fsel(24,BCM2835_GPIO_FSEL_INPT);
+	bcm2835_gpio_set_pud(23,2);
+	bcm2835_gpio_set_pud(24,2);
+	falling_edge(23);
+	falling_edge(24);
+}
+
+void enable_1hz(void)
+{
+	uart_puts("\r\nbegin enable_1hz\r\n");
+	bcm2835_i2c_begin();
+	bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+	bcm2835_i2c_setSlaveAddress(0x68);
+	*ISCN[0] = 0x00;
+	// I2C address 0x68 is the DS3231
+	//mmio_write(0x68, 0x00000000);
+	uart_puts("I hate 525");
+	mmio_write(0xE, 0x00000000);
+	uart_puts("525 sucks");
+	//Psuedo Code Here: clear the INTCN (TY ROCKEY) bit in the DS3231M Control register
+	//bcm2835_delayMicroseconds(1000);
+	uart_puts("\r\nend enable_1hz\r\n");
+	bcm2835_i2c_end();
+}
+
+char read_char_buffer(void)
+{
+	uint8_t c = '\0';
+	while (c == '\0') {c = *cbuf;}
+	//Psuedo Code Here: make cbuf = null
+	cbuf = NULL; //prolly not gonna work but who knows //it didn't work
+	//uart_puts(null);
+	return c;
+}
+
+void banner(void)
+{
+	uart_puts(MS1);
+	uart_puts(MS2);
+}
+
+uint8_t BCDtoUint8(uint8_t BCD)
+{
+	return (BCD & 0x0F) + ((BCD >> 4) * 10);
+}
+
+void DATE(void)
+{
+	uart_puts("\r\nEnter DATE (S)et or (D)isplay\r\n");
+	uint8_t c = '\0';
+	while (c == '\0') 
+	{
+		c = read_char_buffer();
+	}
+	switch (c) {
+		case 'S' | 's':
+			bcm2835_i2c_begin();
+			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+			bcm2835_i2c_setSlaveAddress(0x68);
+			uart_puts("\r\nSet Date\r\n");
+			uart_puts("\r\nType Day of Month (two digits 01-31): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 1 || BCDtoUint8(tens & ones) > 31)
+			{
+				*buffer[0] = 0x01;
+				uart_puts("\r\nInvalid Day of the Month Value!\r\n");
+				break;
+			}
+			else
+			{
+				*buffer[0] = tens & ones;
+			}			
+			bcm2835_i2c_write(DOM,*buffer);
+			uart_puts("\r\nType Month (two digits 01-12): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 1 || BCDtoUint8(tens & ones) > 12) 
+			{
+				*buffer[0] = 0x01;
+				uart_puts("\r\nInvalid Month Value!\r\n");
+				break;
+			}
+			else
+			{
+				*buffer[0] = tens & ones;
+			}			
+			bcm2835_i2c_write(MONTH,*buffer);			
+			uart_puts("\r\nType Year (two digits 00-99): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 99) 
+			{
+				*buffer[0] = 0x00;
+				uart_puts("\r\nInvalid Year Value!\r\n");
+				break;
+			}
+			else
+			{
+				*buffer[0] = tens & ones;
+			}			
+			bcm2835_i2c_write(YEAR,*buffer);			
+			uart_puts("\r\nDate is now set.\r\n");
+			bcm2835_i2c_end();
+            break;
+		case 'D' | 'd':
+			bcm2835_i2c_begin();
+			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+			//Psuedo Code Here: set I2C slave address to 0x68
+			bcm2835_i2c_read(DOM,*buffer);
+			char ones = *buffer[0] & 0x0F;
+			char tens = (*buffer[0] >> 4);
+			uart_putc(tens+48);
+			uart_putc(ones+48);
+			bcm2835_i2c_read(MONTH,*buffer);
+			ones = *buffer[0] & 0x0F;
+			tens = (*buffer[0] >> 4);
+			uart_putc('/');
+			uart_putc(tens+48);
+			uart_putc(ones+48);
+			bcm2835_i2c_read(YEAR,*buffer);
+			ones = *buffer[0] & 0x0F;
+			tens = (*buffer[0] >> 4);
+			uart_putc('/');
+			uart_putc(tens+48);
+			uart_putc(ones+48);	
+			bcm2835_i2c_end();
+			break;
+		default:
+			uart_puts(MS4);
+			DATE();
+			break;
+	}
+}
+
+void TIME(void)
+{
+	uart_puts("\r\nType TIME (S)et or (D)isplay\r\n");
+	uint8_t c = '\0';
+	while (c == '\0') 
+	{
+		c = read_char_buffer();
+	}
+	switch (c) {
+		case 'S' | 's':
+			bcm2835_i2c_begin();
+			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+			bcm2835_i2c_setSlaveAddress(0x68);
+			uart_puts("\r\nSet 24 Hour Time\r\n");
+			uart_puts("\r\nType Hours (two digits 00-23): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 23) 
+			{
+				*buffer[0] = 0x00;
+				uart_puts("\r\nInvalid Hours Value!\r\n");
+				break;
+			}
+			else
+			{
+				*buffer[0] = tens & ones;
+			}			
+			bcm2835_i2c_write(HRS,*buffer);
+			uart_puts("\r\nType Minutes (two digits 00-59): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 59) 
+			{
+				*buffer[0] = 0x00;
+				uart_puts("\r\nInvalid Minutes Value!\r\n");
+				break;
+			}
+			else
+			{
+				*buffer[0] = tens & ones;
+			}			
+			bcm2835_i2c_write(MINS,*buffer);			
+			uart_puts("\r\nType Seconds (two digits 00-59): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 0 || BCDtoUint8(tens & ones) > 59) 
+			{
+				*buffer[0] = 0x00;
+				uart_puts("\r\nInvalid Seconds Value\r\n!");
+				break;
+			}
+			else
+			{
+				*buffer[0] = tens & ones;
+			}			
+			bcm2835_i2c_write(SECS,*buffer);			
+			uart_puts("\r\nTime is now set.\r\n");
+			bcm2835_i2c_end();
+			break;
+            case 'D' | 'd':
+			bcm2835_i2c_begin();
+			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+			bcm2835_i2c_setSlaveAddress(0x68);
+			bcm2835_i2c_read(HRS,*buffer);
+			ones = *buffer[0] & 0x0F;
+			tens = (*buffer[0] >> 4);
+			uart_putc(tens+48);
+			uart_putc(ones+48);
+			bcm2835_i2c_read(MINS,*buffer);
+			ones = *buffer[0] & 0x0F;
+			tens = (*buffer[0] >> 4);
+			uart_putc(':');
+			uart_putc(tens+48);
+			uart_putc(ones+48);
+			bcm2835_i2c_read(SECS,*buffer);
+			ones = *buffer[0] & 0x0F;
+			tens = (*buffer[0] >> 4);
+			uart_putc(':');
+			uart_putc(tens+48);
+			uart_putc(ones+48);	
+			//Psuedo Code Here: End the i2c communication
+			break;	
+		default:
+			uart_puts(MS4);
+			TIME();
+			break;	
+	}
+}
+
+void ALARM(void)
+{
+	uart_puts("\r\nType ALARM (S)et or (D)isplay or (T)est\r\n");
+	uint8_t c = '\0';
+	while (c == '\0') 
+	{
+		c = read_char_buffer();
+	}
+	switch (c) {
+		case 'S' | 's':
+			uart_puts("\r\nSet Seconds Alarm\r\n");
+			uart_puts("\r\nType Starting Alarm Seconds (two digits 05-59): \r\n");
+			tens = ((read_char_buffer()-48) << 4) | 0x0F;
+			uart_putc((tens >> 4)+48);
+			ones = (read_char_buffer()-48) | 0xF0;
+			uart_putc((ones & 0x0F)+48);
+			if (BCDtoUint8(tens & ones) < 5 || BCDtoUint8(tens & ones) > 59) 
+			{
+				alarm[0] = 0x05;
+				uart_puts("\r\nInvalid Alarm Value, Value Reset to 5!\r\n");
+				break;
+			}
+			else
+			{
+				alarm[0] = tens & ones;
+				uart_puts("\r\nAlarm is now set.\r\n");
+			}
+			break;
+		case 'D' | 'd':
+			ones = alarm[0] & 0x0F;
+			tens = alarm[0] >> 4;
+			uart_putc(tens+48);
+			uart_putc(ones+48);
+			break;
+		case 'T' | 't':
+			if (BCDtoUint8(alarm[0]) < 5 || BCDtoUint8(alarm[0]) > 59)
+			{
+				uart_puts("\r\nAlarm Value Out of Range, Set to a Proper Value First!\r\n");
+				break;
+			}
+			uart_puts("\r\nPlease wait, now testing Alarm...\r\n");
+            		bcm2835_i2c_begin();
+			bcm2835_i2c_setClockDivider(BCM2835_I2C_CLOCK_DIVIDER_2500);
+			bcm2835_i2c_setSlaveAddress(0x68);
+			uint32_t restore = mmio_read(0x20200010);
+			//Psuedo Code Here: Configure GPIO40 and 45 for PWM to the 3.5mm Audio Jack
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM_CONTROL, 0x0);//Disable PWM
+			mmio_write(BCM2835_CLOCK_BASE + BCM2835_PWMCLK_CNTL, 0x5A000020); //Stop the PWM clock
+			bcm2835_delayMicroseconds(110);//Long Wait for PWMCLK to settle
+			while ((mmio_read(BCM2835_CLOCK_BASE + BCM2835_PWMCLK_CNTL) & 0x80 != 0)) bcm2835_delayMicroseconds(1);//Wait until clock does stop
+			mmio_write(BCM2835_CLOCK_BASE + BCM2835_PWMCLK_DIV, 0x5A001000);//Set the PWM clock divder to 1.
+			mmio_write(BCM2835_CLOCK_BASE + BCM2835_PWMCLK_CNTL, 0x5A000011);//Enable the PWM clock, make it the 19.2MHZ osc / divider.
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM0_RANGE, 96);//Range0 for left channel is the Cycle Time or Frequency = 1/cycle time
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM1_RANGE, 96);//Range0 for Right channel is the Cycle Time or Frequency = 1/cycle time	
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM0_DATA, 0);//Data0 is the Duty Cycle for the left channel
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM1_DATA, 0);//Data1 is the Duty Cycle for the right channel
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM_CONTROL, BCM2835_PWM0_ENABLE);//turn on left channel
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM_CONTROL, BCM2835_PWM1_ENABLE);//turn on right channel
+			bcm2835_i2c_read(SECS,*buffer);
+			uint8_t initialsecond = BCDtoUint8(*buffer[0]);
+			uint8_t	finalsecond = initialsecond + BCDtoUint8(alarm[0]);
+			if (finalsecond > 59) finalsecond = finalsecond - 60;
+			while (BCDtoUint8(*buffer[0]) < finalsecond) 
+			{
+				bcm2835_i2c_read(SECS,*buffer);
+			}
+			bcm2835_i2c_read(SECS,*buffer);
+			initialsecond = BCDtoUint8(*buffer[0]);
+			finalsecond = initialsecond + 3;
+			if (finalsecond > 59) finalsecond = finalsecond - 60;
+			while (BCDtoUint8(*buffer[0]) < finalsecond) 
+			{
+				for (uint32_t count = 0; count < 200 ; count++) 
+				{
+					mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM0_DATA, N[count]);
+					mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM1_DATA, N[count]);
+					bcm2835_delayMicroseconds(5);
+				}
+				bcm2835_i2c_read(SECS,*buffer);
+			}			
+			bcm2835_i2c_end();
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM0_DATA, 0);//No modulation
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM1_DATA, 0);//No modulation
+			mmio_write(BCM2835_GPIO_PWM + BCM2835_PWM_CONTROL, 0x0);//Disable PWM
+			mmio_write(0x20200010, restore);//restore GPIO40 and 45
+			break;
+		default:
+			uart_puts(MS4);
+			ALARM();
+			break;	
+	}
+}
+
+void RES(void)
+{
+	//Psuedo Code Here: reboot the system
+	// D: based off linked vid in chat
+	//uart_init();
+	//kernel_main(); // D: already initializes stuff in main. TA being cheeky said check boot.s. Stack and stuff initialized there so might need that
+	// D: we need to re set stack which is in boot.s. The reset label in boot.s calls kernel main so i think we need to call that boot.s label here!!!!
+	reboot(); // D: I think this is right this should reset stack pointers. calls reboot in boot.s -> reset in boot.s -> calls main_kernel
+}
+
+void HELP(void) //Command List
+{
+	uart_puts("\r\n(D)ate,(H)elp,a(L)arm,(R)eset,(T)ime,(V)FP11,T(x)");
+}
+
+
+void displaycommandline(void)
+{ 
+	//Engineer the code here to display the active MM/DD/YY:HOURS-MINUTES-SECONDS:TEMPERATURE> command line prompt.
+}
+
+void VFP11(void) //ARM Vector Floating Point Unit Demo
+{
+    
+    //Engineer the VFP11 math coprocessor application here.
+    //Send a menu to hyperterminal as was done with the minimal computer
+    //FADD, FSUB, FMUL, and FDIV, and any other functions you wish to implement in ARM assembly routines in the boot.s file.
+	
+}
+
+void command(void)
+{
+	uart_puts("\r\n");
+	//uart_puts(MS3);
+	uint8_t c = '\0';
+	c = read_char_buffer();
+	uart_puts("The below poster is a chupacabra");
+	uart_putc(c);
+	switch (c) {
+		case 'D' | 'd':
+			DATE();   //DATE command is demo’d
+			break;
+		case 'T' | 't':
+			TIME();   //Time command is demo’d
+			break;
+		case 'L' | 'l':
+			ALARM();  //ALARM command is demo’d
+			break;   
+		case 'R' | 'r':
+			RES();	   //RES command is demo’d
+			break;
+		case 'H' | 'h':
+			HELP();	   //HELP command is demo’d
+			break;
+		case 'V' | 'v':
+			VFP11();    //Vector Floating Point Calculator is demo’d
+			break;
+
+		//Psuedo Code Here: Include the T(x) command for transmitting a large string to Teraterm using Tx interrupt system
+
+		default:
+			if (c != 'q') uart_puts(MS4);
+			HELP();
+			break;
+	}
+}
+
+int logon(void)
+{
+	//Engineer your 3 attempt LOGON code here
+}
+
+void kernel_main() 
+{
+	uart_puts("\r\npoop 1\r\n");
+	uart_init();
+	uart_puts("\r\npoop 2\r\n");
+	enable_irq_57();
+	uart_puts("\r\npoop 3\r\n");
+	enable_irq_52();
+	uart_puts("\r\npoop 4\r\n");
+	enable_1hz();
+	uart_puts("\r\npoop 5\r\n");
+	enable_arm_irq();
+	uart_puts("\r\npoop 6\r\n");
+//	if (logon() == 0) while (1) {}
+	banner();
+	uart_puts("\r\npoop 7\r\n");
+	HELP();
+	uart_puts("\r\npoop 8\r\n");
+	while (1) {
+	    command();
+	}
+	uart_puts("\r\nend while\r\n");
+}
+
+void irq_handler(void)
+{
+	uart_puts("\r\nIRQ HANDLER\r\n");
+	//re-engineer this irq_handler to handle the four interrupts as described in the project 4 document (reset button, 1HZ command line, Rx, Tx)
+
+	uint32_t pending = mmio_read(0x20200040); //read all gpio interrupt flags
+	mmio_write(0x20200040,0xFFFFFFFF);  //clear all gpio interrupt flags
+
+	if ((pending & 0x800000) == 0x800000)  //did the reset button cause the interrupt
+	{
+	   uart_puts("\r\nreboot\r\n");	
+	   reboot();	//if so then warm boot
+	}
+	else if ((pending & 0x1000000) == 0x1000000)
+	{
+           uart_puts("\r\ncommandline\r\n");
+	   displaycommandline();  // if so then update commandline prompt
+	}
+	else
+	{
+	   uart_puts("\r\nread\r\n");
+	   //uart_puts(cbuf); // which one prints
+	   *cbuf = uart_readc(); // if not those then must have been the keyboard
+	   //uart_putc(uart_readc()); // which one prints
+	}
+}
+
+
---
+++ native_template/source/uart.c
@@ -0,0 +1,228 @@
+/* uart.c - UART initialization & communication */
+/* Reference material:
+ * http://www.raspberrypi.org/wp-content/uploads/2012/02/BCM2835-ARM-Peripherals.pdf
+ * Chapter 13: UART
+ */
+ 
+#include <stdint.h>
+#include <mmio.h>
+#include <uart.h>
+ 
+enum 
+{
+    // The GPIO registers base address.
+    GPIO_BASE = 0x20200000,
+ 
+    // The offsets for reach register.
+ 
+    // Controls actuation of pull up/down to ALL GPIO pins.
+    GPPUD = (GPIO_BASE + 0x94),
+ 
+    // Controls actuation of pull up/down for specific GPIO pin.
+    GPPUDCLK0 = (GPIO_BASE + 0x98),
+ 
+    // The base address for UART.
+    UART0_BASE = 0x20201000,
+ 
+    // The offsets for reach register for the UART.
+    UART0_DR     = (UART0_BASE + 0x00),
+    UART0_RSRECR = (UART0_BASE + 0x04),
+    UART0_FR     = (UART0_BASE + 0x18),
+    UART0_ILPR   = (UART0_BASE + 0x20),
+    UART0_IBRD   = (UART0_BASE + 0x24),
+    UART0_FBRD   = (UART0_BASE + 0x28),
+    UART0_LCRH   = (UART0_BASE + 0x2C),
+    UART0_CR     = (UART0_BASE + 0x30),
+    UART0_IFLS   = (UART0_BASE + 0x34), // This should probably be IFSL
+    UART0_IMSC   = (UART0_BASE + 0x38),
+    UART0_RIS    = (UART0_BASE + 0x3C),
+    UART0_MIS    = (UART0_BASE + 0x40),
+    UART0_ICR    = (UART0_BASE + 0x44),
+    UART0_DMACR  = (UART0_BASE + 0x48),
+    UART0_ITCR   = (UART0_BASE + 0x80),
+    UART0_ITIP   = (UART0_BASE + 0x84),
+    UART0_ITOP   = (UART0_BASE + 0x88),
+    UART0_TDR    = (UART0_BASE + 0x8C),
+};
+
+// FFF
+enum {
+    // Data register bits
+    DR_OE = 1 << 11, // Overrun error
+    DR_BE = 1 << 10, // Break error
+    DR_PE = 1 <<  9, // Parity error
+    DR_FE = 1 <<  8, // Framing error
+
+    // Receive Status Register / Error Clear Register
+    RSRECR_OE = 1 << 3, // Overrun error
+    RSRECR_BE = 1 << 2, // Break error
+    RSRECR_PE = 1 << 1, // Parity error
+    RSRECR_FE = 1 << 0, // Framing error
+
+    // Flag Register (depends on LCRH.FEN)
+    FR_TXFE = 1 << 7, // Transmit FIFO empty
+    FR_RXFF = 1 << 6, // Receive FIFO full
+    FR_TXFF = 1 << 5, // Transmit FIFO full
+    FR_RXFE = 1 << 4, // Receive FIFO empty
+    FR_BUSY = 1 << 3, // BUSY transmitting data
+    FR_CTS  = 1 << 0, // Clear To Send
+
+    // Line Control Register
+    LCRH_SPS   = 1 << 7, // sticky parity selected
+    LCRH_WLEN  = 3 << 5, // word length (5, 6, 7 or 8 bit)
+    LCRH_WLEN5 = 0 << 5, // word length 5 bit
+    qLCRH_WLEN6 = 1 << 5, // word length 6 bit
+    LCRH_WLEN7 = 2 << 5, // word length 7 bit
+    LCRH_WLEN8 = 3 << 5, // word length 8 bit
+    LCRH_FEN   = 1 << 4, // Enable FIFOs
+    LCRH_STP2  = 1 << 3, // Two stop bits select
+    LCRH_EPS   = 1 << 2, // Even Parity Select
+    LCRH_PEN   = 1 << 1, // Parity enable
+    LCRH_BRK   = 1 << 0, // send break
+
+    // Control Register
+    CR_CTSEN  = 1 << 15, // CTS hardware flow control
+    CR_RTSEN  = 1 << 14, // RTS hardware flow control
+    CR_RTS    = 1 << 11, // (not) Request to send
+    CR_RXE    = 1 <<  9, // Receive enable
+    CR_TXW    = 1 <<  8, // Transmit enable
+    CR_LBE    = 1 <<  7, // Loopback enable
+    CR_UARTEN = 1 <<  0, // UART enable
+
+    // Interrupts (IMSC / RIS / MIS / ICR)
+    INT_OER   = 1 << 10, // Overrun error interrupt
+    INT_BER   = 1 <<  9, // Break error interrupt
+    INT_PER   = 1 <<  8, // Parity error interrupt
+    INT_FER   = 1 <<  7, // Framing error interrupt
+    INT_RTR   = 1 <<  6, // Receive timeout interrupt
+    INT_TXR   = 1 <<  5, // Transmit interrupt
+    INT_RXR   = 1 <<  4, // Receive interrupt
+    INT_DSRRM = 1 <<  3, // unsupported / write zero
+    INT_DCDRM = 1 <<  2, // unsupported / write zero
+    INT_CTSRM = 1 <<  1, // nUARTCTS modem interrupt
+    INT_RIRM  = 1 <<  0, // unsupported / write zero
+    INT_ALL = 0x7F2,
+
+    IFSL_RXIFLSEL = 7 << 3,     // Receive interrupt FIFO level select
+    IFSL_RX_1_8   = 0b000 << 3, // Receive FIFO 1/8 full
+    IFSL_RX_1_4   = 0b001 << 3, // Receive FIFO 1/4 full
+    IFSL_RX_1_2   = 0b010 << 3, // Receive FIFO 1/2 full
+    IFSL_RX_3_4   = 0b011 << 3, // Receive FIFO 3/4 full
+    IFSL_RX_7_8   = 0b100 << 3, // Receive FIFO 7/8 full
+    IFSL_TXIFLSEL = 7 << 0,     // Transmit interrupt FIFO level select
+    IFSL_TX_1_8   = 0b000 << 0, // Transmit FIFO 1/8 full
+    IFSL_TX_1_4   = 0b001 << 0, // Transmit FIFO 1/4 full
+    IFSL_TX_1_2   = 0b010 << 0, // Transmit FIFO 1/2 full
+    IFSL_TX_3_4   = 0b011 << 0, // Transmit FIFO 3/4 full
+    IFSL_TX_7_8   = 0b100 << 0, // Transmit FIFO 7/8 full
+};
+
+// End FFF
+ 
+/*
+ * delay function
+ * int32_t delay: number of cycles to delay
+ *
+ * This just loops <delay> times in a way that the compiler
+ * wont optimize away.
+ */
+static void delay(int32_t count) 
+{
+   __asm__ volatile("__delay%=: subs %[count], %[count], #1; bne __delay%=\n"
+	     : : [count]"r"(count) : "cc");
+}
+ 
+/*
+ * Initialize UART0.
+ */
+void uart_init() 
+{
+    //uart_puts("\nuart_init\n");
+    // Disable UART0.
+    mmio_write(UART0_CR, 0x00000000);
+    // Setup the GPIO pin 14 && 15.
+ 
+    // Disable pull up/down for all GPIO pins & delay for 150 cycles.
+    mmio_write(GPPUD, 0x00000000);
+    delay(150);
+ 
+    // Disable pull up/down for pin 14,15 & delay for 150 cycles.
+    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+ 
+    // Write 0 to GPPUDCLK0 to make it take effect.
+    mmio_write(GPPUDCLK0, 0x00000000);
+ 
+    // Clear pending interrupts.
+    mmio_write(UART0_ICR, 0x7FF);
+ 
+    // Set integer & fractional part of baud rate.
+    // Divider = UART_CLOCK/(16 * Baud)
+    // Fraction part register = (Fractional part * 64) + 0.5
+    // UART_CLOCK = 3000000; Baud = 115200.
+ 
+    // Divider = 3000000/(16 * 115200) = 1.627 = ~1.
+    // Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
+    mmio_write(UART0_IBRD, 1);
+    mmio_write(UART0_FBRD, 40);
+ 
+    // Disable FIFO. Make 8 bit data transmission (1 stop bit, no parity).
+    mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+ 
+    // Engineer the Interrupt for UART0 Receive // D: save state here somehow. I think he does something similar elsewhere in the code
+    //mmio_write(UART0_IMSC, 0x0010);
+
+    mmio_write(UART0_IFLS, IFSL_RX_1_2 | IFSL_TX_1_2);
+
+    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | /*(1 << 5) |*/ (1 << 6) 
+			   /*| (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10)*/);
+ 
+    // Enable UART0, receive & transfer part of UART.
+    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+}
+ 
+/*
+ * Transmit a byte via UART0.
+ * uint8_t Byte: byte to send.
+ */
+void uart_putc(uint8_t byte) // D: pg179 of arm pdf. If FIFO is enabled data written to this location is pushed onto the transmit FIFO. OE overrun error bit 11 set to 1 if the fifo already full and receives an input
+// D: if the fifo's are enabled the data byte and the 4 bit status is pushed onto the 12 bit wide receive fifo
+{
+    // test for UART to become ready to transmit
+    while (1) 
+	{
+        if (!(mmio_read(UART0_FR) & (1 << 5))) 
+		{
+	    break;
+		}
+    }
+    mmio_write(UART0_DR, byte);
+}
+
+uint8_t uart_readc(void)
+{
+	//uart_puts("\r\nuart_readc\r\n");
+	// test for UART to become ready to read, can also test the interrupt receive flag.
+	while (1) 
+	{
+        if (!(mmio_read(UART0_FR) & (1 << 4))) 
+		{
+		//uart_puts("\r\nIF STATEMENT WTF\r\n");
+			break;
+		}
+    	}
+	//uart_puts("\r\nend uart_readc\r\n");
+	return mmio_read(UART0_DR);
+}
+ 
+/*
+ * print a string to the UART one character at a time
+ * const char *str: 0-terminated string
+ */
+void uart_puts(const char *str) 
+{
+    while (*str) 
+	{
+        uart_putc(*str++);
+    	}
+}
